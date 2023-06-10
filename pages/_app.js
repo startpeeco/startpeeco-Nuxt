965565bd9c59d98991e74a7c82c5000902a3e191
startpeeco <134424987+startpeeco@users.noreply.github.com>
2023-06-10 19:32:21 -0300

    Delete pages directory

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-
-import React from "react";
-export default function MyApp({
-  Component: Component,
-  pageProps: pageProps
-}) {
-  return <Component {...pageProps} />;
-}
